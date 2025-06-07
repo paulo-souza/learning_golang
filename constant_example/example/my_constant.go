93ef3b27bd410f994b219ad7ba495acd4be86f2b
paulo-souza <phespecialmaster@gmail.com>
2025-06-06 22:11:25 -0300

    feat(constant_example): Exemplificar terceira forma de criar constantes.

--- constant_example/example/my_constant.go
+++ constant_example/example/my_constant.go
@@ -20,3 +20,12 @@ func SecondForm() {
 	const pi, eulerNumber float32 = 3.1415, 2.71828
 	fmt.Printf("pi = %.4f\ne = %f\n", pi, eulerNumber)
 }
+
+func ThirdForm() {
+	const (
+		eightBits byte    = 8
+		oneKbyte  int     = 1024
+		pi        float32 = 3.1415
+	)
+	fmt.Printf("1 byte = %d bits\n1 Kb = %d bytes\npi = %.4f\n", eightBits, oneKbyte, pi)
+}
