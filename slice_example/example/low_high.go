76fbdc74adf6fd6712a6f13c258482a16aec28c6
paulo-souza <phespecialmaster@gmail.com>
2025-06-11 19:32:12 -0300

    feat(slice_example): Exemplicar cuidados ao utilizar "low" e "high".
    Estes cuidados envolve o vínculo que um slice criado utilizando "low" e "high", pois uma vez alterado o slice ou array ascendente, toda essa alteração é refletida em seus descendentes.

--- slice_example/example/low_high.go
+++ slice_example/example/low_high.go
@@ -5,8 +5,8 @@ import "fmt"
 var beatles = []string{"Paul", "Ringo", "George", "John"}
 
 /*
-	"Low" and "High" is a way of extracting a slice from a slice or matrix while maintaining a link to them.
-	"Low" is the first index of the slice and "High" is the last (but "High" does not include the index itself).
+"Low" and "High" is a way of extracting a slice from a slice or matrix while maintaining a link to them.
+"Low" is the first index of the slice and "High" is the last (but "High" does not include the index itself).
 */
 func ExampleOfLowAndHigh() {
 	paulAndRingo := beatles[0:2]
@@ -21,4 +21,17 @@ func ExampleOfLowAndHigh() {
 
 	fmt.Println(viewBeatles)
 	fmt.Println(allBeatles)
+}
+
+// Be careful when using "low" and "high".
+func WarningWithLowAndHigh() {
+	allBeatles := beatles[:]
+	fmt.Println("beatles =>", beatles)
+	fmt.Println("allBeatles =>", allBeatles)
+
+	beatles[0] = "Elvis"
+	beatles[2] = "Madonna"
+
+	fmt.Println("\nbeatles =>", beatles)
+	fmt.Println("allBeatles =>", allBeatles)
 }
--- slice_example/main.go
+++ slice_example/main.go
@@ -5,5 +5,5 @@ import (
 )
 
 func main() {
-	example.ExampleOfLowAndHigh()
+	example.WarningWithLowAndHigh()
 }
