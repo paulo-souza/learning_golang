6a66f51a548ad6a32d80d6f4c018a95c325a649a
paulo-souza <phespecialmaster@gmail.com>
2025-06-11 20:03:46 -0300

    feat(slice_example): Exemplificando o uso de copy.
    Neste exemplo além de demonstrar como utilizar a função "copy", também é apresentado uma das soluções para problemas de alterações ocasionados por "low" e "high".

--- slice_example/example/low_high.go
+++ slice_example/example/low_high.go
@@ -34,4 +34,17 @@ func WarningWithLowAndHigh() {
 
 	fmt.Println("\nbeatles =>", beatles)
 	fmt.Println("allBeatles =>", allBeatles)
+	fmt.Println("\nOBS.: Para solucionar este problema pode se fazer uso de \"copy\" ou \"append\"")
+}
+
+// Example of using copy and one of the solutions to problems that "low" and "high" can cause.
+func UsingCopy() {
+	var originalBeatles []string = make([]string, 4)
+	copy(originalBeatles, beatles)
+
+	beatles[0] = "Elvis"
+	beatles[2] = "Madonna"
+
+	fmt.Println("beatles =>", beatles)
+	fmt.Println("originalBeatles =>", originalBeatles)
 }
