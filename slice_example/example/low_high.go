3e8168f8c7e533b14995c1c220f91e59733deb74
paulo-souza <phespecialmaster@gmail.com>
2025-06-12 13:03:03 -0300

    feat(slice_example): Exemplicar o uso de append.
    Além de exemplicar  a utilização de append, também é aprofundado coisas interessantes como o tamanho e a capacidade de um slice, pois este sempre possuirá uma referência para um array oculto.

--- slice_example/example/low_high.go
+++ slice_example/example/low_high.go
@@ -48,3 +48,43 @@ func UsingCopy() {
 	fmt.Println("beatles =>", beatles)
 	fmt.Println("originalBeatles =>", originalBeatles)
 }
+
+// Example of using append and one of the solutions to problems that "low" and "high" can cause.
+func UsingAppend() {
+	newBeatles := append(beatles, "Elvis", "Madonna")
+	beatles[2] = "Michael"
+	fmt.Println("beatles =>", beatles)
+	fmt.Println("newBeatles =>", newBeatles)
+}
+
+// Digging deeper into the use of append and demonstrating interesting things about it.
+func OtherInterestingThingsAboutAppend() {
+	sliceExample := make([]int, 3, 5)
+	sliceExample[0] = 21
+	sliceExample[1] = 5
+	sliceExample[2] = 9
+	fmt.Println("sliceExample =", sliceExample)
+	fmt.Println("Tamanho de sliceExample:", len(sliceExample))
+	fmt.Println("Capacidade de sliceExample:", cap(sliceExample))
+
+	//Adds elements 90 and 77 in "sliceExample" and only changes its size
+	sliceExample = append(sliceExample, 90, 77)
+	fmt.Println("\nsliceExample =", sliceExample)
+	fmt.Println("Tamanho de sliceExample:", len(sliceExample))
+	fmt.Println("Capacidade de sliceExample:", cap(sliceExample))
+
+	//Creates and assigns a new slice in "sliceExample" with new size and new capacity
+	sliceExample = append(sliceExample, 13, 43, 69)
+	fmt.Println("\nsliceExample =", sliceExample)
+	fmt.Println("Tamanho de sliceExample:", len(sliceExample))
+	fmt.Println("Capacidade de sliceExample:", cap(sliceExample))
+
+	/*
+		Copies the elements of "sliceExample" to a new slice and adds more elements to the end of it,
+		such as 90, 77, 3 and 1, and then assigns them to the variable "otherSlice"
+	*/
+	otherSlice := append(sliceExample, 90, 77, 3, 1)
+	fmt.Println("\notherSlice =", otherSlice)
+	fmt.Println("Tamanho da otherSlice:", len(otherSlice))
+	fmt.Println("Capacidade da otherSlice:", cap(otherSlice))
+}
--- slice_example/main.go
+++ slice_example/main.go
@@ -5,5 +5,5 @@ import (
 )
 
 func main() {
-	example.UsingCopy()
+	example.OtherInterestingThingsAboutAppend()
 }
