2d9799a570b861af4bb03a0614c082a86600c4c5
paulo-souza <phespecialmaster@gmail.com>
2025-06-21 19:53:25 -0300

    feat(loop_example): Exemplificar primeira forma de loop.

--- loop_example/example/my_loops.go
+++ loop_example/example/my_loops.go
@@ -1,5 +1,16 @@
 package example
 
-func FirstForm() string {
-	return "OK! -- FirstForm --"
+import "fmt"
+
+/*
+	Every repetition structure in Golang is defined with the reserved word "for",
+	and there are several ways to do this, below are the main ones.
+*/
+
+func FirstForm() {
+	count := 1
+	for count <= 10 {
+		fmt.Println(count)
+		count = count + 1
+	}
 }
--- loop_example/main.go
+++ loop_example/main.go
@@ -1,12 +1,9 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/paulo-souza/learning_golang/loop_example/example"
 )
 
 func main() {
-	var msg string = example.FirstForm()
-	fmt.Println(msg)
+	example.FirstForm()
 }
